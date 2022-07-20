cec7c1b0c962349948d59c0c62b4131d78a11b46
noppefoxwolf <tomoya.hirano@dena.com>
2022-07-20 20:29:55 +0900

    Add Example

---
+++ Example.swiftpm/ContentView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import Kingfisher
+import KingfisherWebP
+
+struct ContentView: View {
+    
+    init() {
+        KingfisherManager.shared.cache.clearCache()
+        KingfisherManager.shared.defaultOptions += [
+            .processor(WebPImageProcessor.default),
+            .cacheSerializer(WebPCacheSerializer.default)
+        ]
+    }
+    
+    var body: some View {
+        KFAnimatedImage(Bundle.main.url(forResource: "animated-webp-supported", withExtension: "webp"))
+            .frame(width: 120, height: 120)
+    }
+}
+
